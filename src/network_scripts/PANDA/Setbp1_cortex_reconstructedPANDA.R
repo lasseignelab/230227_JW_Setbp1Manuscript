f09078c74884c0b5a1d4cdf964a6fcf776ccff86
jordanwhitlock <jwhitlock@uab.edu>
2023-03-23 23:11:45 -0500

    updating file paths and packages and cleaning up script for review

--- src/network_scripts/PANDA/Setbp1_cortex_reconstructedPANDA.R
+++ src/network_scripts/PANDA/Setbp1_cortex_reconstructedPANDA.R
@@ -1,21 +1,21 @@
-#set up libPath. NOTE: THE USER MUST UNCOMMENT AND ADD THEIR USERNAME AND COMMENT OUT MINE FOR LIBPATHS
+#set up libPath. NOTE: THE USER MUST UNCOMMENT AND ADD THEIR USERNAME AND COMMENT OUT MINE FOR LIBPATHS. 
 ##set user
-user <- "~/jbarham3"
+username <- "jbarham3"
 #user <- "~/YOUR_USER" #change this
 
 ##give libpath
-libpath <- "R/x86_64-pc-linux-gnu-library/slurm_4.2/"
+libpath <- paste0("/data/user/home/", username, "/R/x86_64-pc-linux-gnu-library/4.2") #must use this when submitting jobs through SLURM with R version 4.2, if you are not using 4.2 then the libpath needs to be changed
 
-.libpaths(paste0(user,libpath, sep = "/")) #must use this when submitting jobs through SLURM with R version 4.2, if you are not using 4.2 then the libpath needs to be changed
+.libPaths(libpath)
 
-##install packages and load
-install_github(repo = "netZoo/netZooR", ref = "master")
+##install packages; note this is done once in either the kidney or brain script and then commented out in the other and for all future re-runs (it takes almost 2 hours to install the packages)
+#remotes::install_github(repo = "netZoo/netZooR", ref = "master")
+#install.packages("data.table")
+#install.packages("here")
+
+#load in package libraries
 library(netZooR)
-
-install.packages("data.table")
 library(data.table)
-
-install.packages("here")
 library(here)
 
 #set up environment 
@@ -27,11 +27,11 @@ source(here("src/functions/functions.R"))
 #load in the input data needed:
 motif <- read.table(file = here("data/processed/motif_inputs/mus_motif_all.txt"), sep = "\t") #load in motif data
 
-ppi <- read.table(file = here("data/inputs/processed/ppi_inputs/mm10_ppi.txt"), sep = "\t") #load in ppi data
+ppi <- read.table(file = here("data/processed/ppi_inputs/mm10_ppi.txt"), sep = "\t") #load in ppi data
 
 #make list of expression files needed to loop through
-files <- list.files(here("data/processed/expression_inputs/"), pattern = "expression.Rdata", full.names = TRUE)
-files <- files[c(1,2,17,18,23,24,29,30,31,32,33,34,35,36)]#grab all cerebral samples
+files <- list.files(here("data/processed/expression_inputs"), pattern = "expression.Rdata", full.names = TRUE)
+files <- files[c(19, 20, 23, 24, 25, 26, 29, 30, 33, 34, 35, 36, 37, 38, 39, 40)]#grab all cerebral samples
 
 
 #run for loop
@@ -40,9 +40,9 @@ for(i in files){
   fileName = i
   expression <- loadRData(fileName)
   #run panda on multi-omic inputs
-  set.seed(1234)
+  set.seed(2178)
   pandaResults <- makePanda(motif, ppi, expression)
   name <- sub(".Rdata", "", basename(i))
-  save(pandaResults, file = paste0(name, "_PANDA.Rdata"))
+  save(pandaResults, file = paste0(here("results/PANDA/"), name, "_PANDA.Rdata"))
   print(paste0(name, "_PANDA network made and saved."))
 }
--- src/network_scripts/PANDA/Setbp1_kidney_reconstructedPANDA.R
+++ src/network_scripts/PANDA/Setbp1_kidney_reconstructedPANDA.R
@@ -1,21 +1,21 @@
-#set up libPath. NOTE: THE USER MUST UNCOMMENT AND ADD THEIR USERNAME AND COMMENT OUT MINE FOR LIBPATHS
+#set up libPath. NOTE: THE USER MUST UNCOMMENT AND ADD THEIR USERNAME AND COMMENT OUT MINE FOR LIBPATHS. 
 ##set user
-user <- "~/jbarham3"
+username <- "jbarham3"
 #user <- "~/YOUR_USER" #change this
 
 ##give libpath
-libpath <- "R/x86_64-pc-linux-gnu-library/slurm_4.2/"
+libpath <- paste0("/data/user/home/", username, "/R/x86_64-pc-linux-gnu-library/4.2") #must use this when submitting jobs through SLURM with R version 4.2, if you are not using 4.2 then the libpath needs to be changed
 
-.libpaths(paste0(user,libpath, sep = "/")) #must use this when submitting jobs through SLURM with R version 4.2, if you are not using 4.2 then the libpath needs to be changed
+.libPaths(libpath)
 
-##install packages and load
-install_github(repo = "netZoo/netZooR", ref = "master")
+##install packages; note this is done once in either the kidney or brain script and then commented out in the other and for all future re-runs (it takes almost 2 hours to install the packages)
+#remotes::install_github(repo = "netZoo/netZooR", ref = "master")
+#install.packages("data.table")
+#install.packages("here")
+
+#load in package libraries
 library(netZooR)
-
-install.packages("data.table")
 library(data.table)
-
-install.packages("here")
 library(here)
 
 #set up environment 
@@ -27,11 +27,11 @@ source(here("src/functions/functions.R"))
 #load in the input data needed:
 motif <- read.table(file = here("data/processed/motif_inputs/mus_motif_all.txt"), sep = "\t") #load in motif data
 
-ppi <- read.table(file = here("data/inputs/processed/ppi_inputs/mm10_ppi.txt"), sep = "\t") #load in ppi data
+ppi <- read.table(file = here("data/processed/ppi_inputs/mm10_ppi.txt"), sep = "\t") #load in ppi data
 
 #make list of expression files needed to loop through
-files <- list.files(here("data/processed/expression_inputs/"), pattern = "expression.Rdata", full.names = TRUE)
-files <- files[c(3,4,5,6,7,8,9,10,11,12,13,14,15,16,19,20,21,22,25,26,27,28,37,38,39,40,41,42,43,44,45,46)]#grab all kidney samples
+files <- list.files(here("data/processed/expression_inputs"), pattern = "expression.Rdata", full.names = TRUE)
+files <- files[c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 27, 28, 31, 32, 41, 42, 43, 44)]#grab all kidney samples
 
 
 #run for loop
@@ -40,10 +40,10 @@ for(i in files[2:32]){
   fileName = i
   expression <- loadRData(fileName)
   #run panda on multi-omic inputs
-  set.seed(1235)
+  set.seed(2178)
   pandaResults <- makePanda(motif, ppi, expression)
   name <- sub(".Rdata", "", basename(i))
-  save(pandaResults, file = paste0(name, "_PANDA.Rdata"))
+  save(pandaResults, file = paste0(here("results/PANDA/"), name, "_PANDA.Rdata"))
   rm(pandaResults)
   print(paste0(name, "_PANDA network made and saved."))
 }
