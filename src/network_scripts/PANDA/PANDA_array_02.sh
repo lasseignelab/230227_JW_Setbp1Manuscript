027da7f1a9b50cd436fd6764206c66ae0f37e6b7
jordanwhitlock <jwhitlock@uab.edu>
2023-05-03 10:19:40 -0500

    array script

--- src/network_scripts/PANDA/PANDA_array_02.sh
+++ src/network_scripts/PANDA/PANDA_array_02.sh
@@ -27,13 +27,19 @@
 module load Singularity/3.5.2-GCC-5.4.0-2.26
 
 #variables
-wd="$USER_DATA/230227_JW_Setbp1Manuscript"
-src="$USER_DATA/230227_JW_Setbp1Manuscript/src/network_scripts/PANDA" #be sure that your subdirectories are structured the same
+wd="/data/user/jbarham3/230227_JW_Setbp1Manuscript"
+src="/data/user/jbarham3/230227_JW_Setbp1Manuscript/src/network_scripts/PANDA" #be sure that your subdirectories are structured the same
+
+export SINGULARITYENV_PASSWORD='pass'
+export SINGULARITYENV_USER='user'
 
 #code to execute docker and script for analysis
 cd ${wd}
 
-#array of cell type specific expression inputs for PANDA
+#array file of cell type specific expression inputs for PANDA
 SAMPLE_LIST="${wd}/results/array_inputs/Setbp1_PANDA_files.txt" #note: make sure path and file name are correct
+SAMPLE_ARRAY=(`cat ${SAMPLE_LIST}`) # parantheses instruct bash to create a shell array of strings from SAMPLE_LIST
+INPUT=`echo ${CELL_LIST[$SLURM_ARRAY_TASK_ID]}` #extracts a single input from the array and prints (using echo) it into INPUT variable, each single input is then assigned an array number by $SLURM_TASK_ID
 
-singularity exec -B ${src} ${wd}/bin/PANDA_docker/setbp1_manuscript_panda_1.0.0_latest.sif Rscript --vanilla ${src}/PANDA.R ${SAMPLE_LIST[$SLURM_ARRAY_TASK_ID]} # here vanilla ensures only the script is run and environment is kept clean
+
+singularity exec --cleanenv --no-home -B ${wd} ${wd}/bin/PANDA_docker/setbp1_manuscript_panda_1.0.0_latest.sif Rscript --vanilla ${src}/PANDA.R ${INPUT} # here vanilla ensures only the script is run and environment is kept clean
