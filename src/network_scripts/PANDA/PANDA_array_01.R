2335566a6f7170a1d4955409a399e6b45e4b3d94
jordanwhitlock <jwhitlock@uab.edu>
2023-05-10 09:49:59 -0500

    adding docker container informati9on

--- src/network_scripts/PANDA/PANDA_array_01.R
+++ src/network_scripts/PANDA/PANDA_array_01.R
@@ -1,11 +1,12 @@
-# NOTE: be sure to run this script to set up list of files BEFORE constructing networks with 'PANDA_array_02.sh' bash script.
+# NOTE: be sure to run this script to set up list of files BEFORE constructing networks with 'PANDA_array_02.sh' bash script. 
+# This script was run inside of JW docker container jordanwhitlock/setbp1_manuscript:1.0.5 (setbp1_manuscript_1.0.5.sif) using Singularity/3.5.2-GCC-5.4.0-2.26 
 set.seed(2178)
 
 ##Load libraries
 library(here)
 library(utils)
 
-##Make list of .Rdata expression files needed for array job:
+##Make list of .Rdata expression files needed for array job (generated using `MouseSetbp1_PANDA_expression_05.Rmd`):
 files <- list.files(here("data/processed/expression_inputs"), full.names = TRUE) # ensure that ALL exppression inputs for PANDA are in the same directory and that nothing else is in the directory.
 files #save this as a text file to be the array file
 
