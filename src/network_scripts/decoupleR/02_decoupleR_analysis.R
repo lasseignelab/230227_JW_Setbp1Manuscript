0a2dd8b87f4c1039db7134ddb4231f6992636f22
jordanwhitlock <jwhitlock@uab.edu>
2023-05-26 10:19:37 -0500

    changing decoupleR prior and expression input names

---
+++ src/network_scripts/decoupleR/02_decoupleR_analysis.R
@@ -0,0 +1,42 @@
+#!/usr/bin/env Rscript
+
+# Load necessary libraries
+library(dplyr)
+library(decoupleR)
+
+# Fetch command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+rdata_file <- args[1]
+prior_net <- args[2]
+cell_type <- args[3]
+tissue <- args[4]
+min_n <- as.numeric(args[5])
+
+print(rdata_file)
+print(prior_net)
+print(cell_type)
+print(tissue)
+print(min_n)
+
+
+
+load(file = rdata_file)
+mat <- as.matrix(data.frame(decoupleR_expression[[cell_type]]))
+net <- data.frame(read.csv(prior_net), header = TRUE)
+
+# Define function
+tf_activity <- function(mat, net, cell_type, min_n, tissue){
+  acts <- run_mlm(mat, net, .source = "source", .target = "target", .mor = "mor", minsize = min_n) %>% mutate(cell_type = cell_type)
+  #summarize_acts <- acts %>% group_by(source) %>% summarise(mean = mean(score)) %>% mutate(cell_type = cell_type)
+  return(acts)
+}
+
+# Run function and save results
+acts <- tf_activity(mat, net, cell_type, min_n, tissue)
+
+# Specify output path
+output_path <- paste("/data/user/jbarham3/230227_JW_Setbp1Manuscript/results/decoupleR/", cell_type, "_acts.RData", sep="")
+
+# Save the output
+save(acts, file = output_path)
---
+++ src/network_scripts/decoupleR/02_decoupleR_array_job.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+## run the Rscript decoupleR_analysis.R and schedule this job to SLURM with
+## `sbatch decoupleR_array_job.sh`
+
+#SBATCH --job-name=kid_decoupleR #Note: when running kidney this needs to be changed to 'kid_decoupleR' from 'cor_decoupleR'
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jbarham3@uab.edu #CHANGE THE EMAIL TO YOUR OWN
+#
+#Number of tasks needed for this job, the partition, and parameters
+#SBATCH --ntasks=1
+#SBATCH --mem-per-cpu=255000
+#SBATCH --nodes=1
+#SBATCH --time=120:00:00 #Note: '24:00:00 hours for brain, when running kidney this needs to be changed to 5 days as '120:00:00'
+#SBATCH --partition=largemem-long #partition info here: https://docs.rc.uab.edu/cheaha/hardware/#partitions
+#
+#SBATCH --output=%x_%A_%a.out
+#SBATCH --error=%x_%A_%a.err
+#SBATCH --array=0-33 #Note: when running kidney this needs to be changed to 0-33 from 0-15 for brain; 16 dfs in list for cortex and 34 for kidney
+
+########################################
+### PUT YOUR COMMANDS BELOW THIS BOX ###
+########################################
+module load R
+module load Singularity/3.5.2-GCC-5.4.0-2.26
+
+wd=/data/user/jbarham3/230227_JW_Setbp1Manuscript #change this to match your project directory path
+#exp="/data/project/lasseigne_lab/JordanWhitlock/230227_Setbp1Manuscript/processed_data/decoupleR_expression_inputs" #be sure that your subdirectories are structured the same
+
+export SINGULARITYENV_PASSWORD='pass'
+export SINGULARITYENV_USER='jbarham3' #change this to your user
+
+
+cd ${wd}
+
+#input expression .Rdata file
+RDATA_FILE=${wd}/data/processed/decoupleR_expression_inputs/kidney_expression.Rdata # NOTE: be sure to swap out 'cerebral_expression' for 'kidney_expression', or other tissue of interest and resubmit this same job
+echo "Opening ${RDATA_FILE}"
+
+#input prior network file
+PRIOR_NET=${wd}/data/processed/decoupleR_prior_CollecTRI/mouse_prior_tri.csv
+echo "Prior Network from ${PRIOR_NET}"
+
+ITEM=$(Rscript -e "load('${RDATA_FILE}'); cat(names(decoupleR_expression)[${SLURM_ARRAY_TASK_ID}])")
+echo "Processing: ${ITEM}"
+
+TISSUE="brain"
+MIN_N=5
+
+singularity exec --cleanenv --no-home -B ${wd} ${wd}/bin/docker/setbp1_manuscript_1.0.6.sif Rscript --vanilla ${wd}/src/decoupleR/02_decoupleR_analysis.R ${RDATA_FILE} ${PRIOR_NET} ${ITEM} ${TISSUE} ${MIN_N}
